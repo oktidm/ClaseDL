f58bd67cc9d3aded45149144aab0de06d4975801
Okti <oktidm@gmail.com>
2015-06-17 19:41:44 -0500

    commit clase anterior

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :post
+	belongs_to :user
+	belongs_to :post
+	belongs_to :votable, polymorphic: true
+	validates :user_id, uniqueness: { scope: :post}
 
-  validates :user_id, uniqueness: { scope: :post}
+
+
 end
